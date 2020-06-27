01b8164bea708bcb4eb38d940c3faefef3ce8d84
Asror <asrorjon.maxmudov@mail.ru>
2020-06-27 20:26:22 +0300

    Bu mening birinchi commit qilishim

---
+++ script.js
@@ -0,0 +1,75 @@
+'use strict';
+
+ 
+/* 
+var leftBorderWidth = 1;
+{
+    let second =2;
+}
+
+console.log(second);  1 - lesson */
+
+/* Тип данных */
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var bolean = true;
+// null;
+// undefined; 
+// var obj = {};
+
+// console.log(4/0);
+// console.log("string"*9); lesson - 2
+
+// console.log(a);
+/* to - string */
+
+// 1) string turini xosil qilish yoki istalgan turni string ga o'zgartirish
+// console.log(typeof (String(4)));
+// 2) 
+// Kankatinatsiya - ya'ni "string" turini xosil qilish boshqa turni qo'shganda xam
+
+// console.log(typeof (String("www" + 5)));
+// console.log(typeof (String("www" + false)));
+// console.log('https://vk.com/catalog/' + 5);
+
+
+/* to - number */
+// 1) 
+// console.log(typeof(Number('5')));
+// 2)
+// console.log(typeof(5 + +'5'));
+// 3)
+// console.log(typeof(parseInt('15', 10)));
+
+// Foydalanuvchilardan keladigan xabarlar doimo "string" turida bo'ladi Ulardni "number" ko'rinishiga o'tkazish uchun quyidagi ishni qilish kere
+
+// let answer = +prompt('Hello', '');
+
+
+// Endi Mantiqiy turdagilarga o'tamiz. Quyidagilar doimo "false" qiymatni qabul qiladi
+// NaN, undefined, null,  '', 0
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working..');
+// }
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working..');
+// }
+
+
+// Foydalanuvchilar bilan aloqa qilish komandalari
+// 1)
+// alert('Hello World!!!');
+// 2)
+// confirm('Are you here?');
+
+// let answers = confirm('Are you here?');
+// console.log(answers);
+
+// let answers = prompt('Are you here?');
+// console.log(answers);
